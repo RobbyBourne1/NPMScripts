b5f025b00e3f2c669a8907b6d1d1665e5e15be0f
Robby Bourne <robbybourne1@gmail.com>
2017-07-27 08:25:06 -0400

    NPM Script Template Package

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, resp) => {
+  resp.send('Hello World')
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
